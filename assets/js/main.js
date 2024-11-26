5cb80b71487f0be010e8a0d5b85a4f7fd2ce0015
Eihab Obeidat <obeidateihab9@gmail.com>
2024-11-26 22:09:20 +0300

    init setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,22 +12,22 @@
   }
 
   // ======= Sticky
-  window.onscroll = function () {
+  function onLoad() {
     const header_navbar = document.querySelector(".navbar-area");
     const sticky = header_navbar.offsetTop;
     const logo = document.querySelector(".navbar-brand img");
 
-  if (window.pageYOffset > sticky) {
-    header_navbar.classList.add("sticky");
-    logo.src = "assets/images/logo/Asset 2.png";
-    logo.style.width = '220px'; // Adjust the width to your desired size
-    logo.style.height = 'auto'; // Keeps the aspect ratio
-} else {
-    header_navbar.classList.remove("sticky");
-    logo.src = "assets/images/logo/scfifile white.png";
-    logo.style.width = '220px'; // Adjust the width to your desired size for the non-sticky state
-    logo.style.height = 'auto'; // Keeps the aspect ratio
-}
+    if (window.pageYOffset > sticky) {
+      header_navbar.classList.add("sticky");
+      logo.src = "assets/images/logo/Asset 2.png";
+      logo.style.width = "220px"; // Adjust the width to your desired size
+      logo.style.height = "auto"; // Keeps the aspect ratio
+    } else {
+      header_navbar.classList.remove("sticky");
+      logo.src = "assets/images/logo/scfifile white.png";
+      logo.style.width = "220px"; // Adjust the width to your desired size for the non-sticky state
+      logo.style.height = "auto"; // Keeps the aspect ratio
+    }
 
     // show or hide the back-top-top button
     const backToTop = document.querySelector(".back-to-top");
@@ -39,7 +39,7 @@
     } else {
       backToTop.style.display = "none";
     }
-  };
+  }
 
   // ==== for menu scroll
   const pageLink = document.querySelectorAll(".page-scroll");
@@ -341,4 +341,8 @@
       },
       retina_detect: !0,
     });
+
+  onLoad();
+
+  window.onscroll = onLoad;
 })();
